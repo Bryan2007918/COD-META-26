653486c32156397a6b22c2a6e16c9ce0408549a4
Bryan Jesus <bryan.delgado1007@gmail.com>
2026-03-30 16:20:28 -0600

    Initialize Firebase app and export auth and db

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBZQH7fbt_PHXQpJU_mk6bgBING7g0eOPw",
+  authDomain: "cod-meta-26.firebaseapp.com",
+  projectId: "cod-meta-26",
+  storageBucket: "cod-meta-26.firebasestorage.app",
+  messagingSenderId: "224615162333",
+  appId: "1:224615162333:web:fb49037c4fbd8c1cc9d634"
+};
+
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
